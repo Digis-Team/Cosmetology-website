05586c4e4d20ff906afb69de05b7279c5fde44c5
olhavalchak <o.valchak@do-it.co>
2021-10-08 14:33:16 +0300

    fixed slider-carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, SliderCarousel } from '../components';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export const Home = ()=> {
     return(
