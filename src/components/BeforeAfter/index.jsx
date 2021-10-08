ecbefc5a70118dabd649b240d7b86584c07f2acc
olhavalchak <o.valchak@do-it.co>
2021-10-08 15:49:32 +0300

    fixed line in beforeAfter

--- src/components/BeforeAfter/index.jsx
+++ src/components/BeforeAfter/index.jsx
@@ -1,34 +1,34 @@
-import React from "react";
+import React from 'react';
 import {
     ReactCompareSlider,
     ReactCompareSliderImage
-  } from "react-compare-slider";
-import { BA_PICTURES } from "../../constants";
+  } from 'react-compare-slider';
+import { BEFORE_AFTER_PICTURES } from '../../constants';
 
   export const BeforeAfter = () => {
     return(
       <div className="before-after-smth">
-        <div className="before-after-text">__________________________________________________________</div>
+        <div className="before-after-text line"></div>
         <h1 className="before-after-text">Before/After</h1>
         <div className="before-after-slider-parent">
           <div className="before-after-slider">
             <ReactCompareSlider className="before-after-photos"
-              itemOne={<ReactCompareSliderImage src={BA_PICTURES[0]} alt="Image one" />}
-              itemTwo={<ReactCompareSliderImage src={BA_PICTURES[1]} alt="Image two" />}
+              itemOne={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[0]} alt="Image one" />}
+              itemTwo={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[1]} alt="Image two" />}
             />
             <div className="before-after-text">photo1</div>
           </div>
           <div className="before-after-slider">
             <ReactCompareSlider className="before-after-photos"
-              itemOne={<ReactCompareSliderImage src={BA_PICTURES[2]} alt="Image one" />}
-              itemTwo={<ReactCompareSliderImage src={BA_PICTURES[3]} alt="Image two" />}
+              itemOne={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[2]} alt="Image one" />}
+              itemTwo={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[3]} alt="Image two" />}
             />
             <div className="before-after-text">photo2</div>
           </div>
           <div className="before-after-slider">
             <ReactCompareSlider className="before-after-photos"
-              itemOne={<ReactCompareSliderImage src={BA_PICTURES[4]} alt="Image one" />}
-              itemTwo={<ReactCompareSliderImage src={BA_PICTURES[5]} alt="Image two" />}
+              itemOne={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[4]} alt="Image one" />}
+              itemTwo={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[5]} alt="Image two" />}
             />
             <div className="before-after-text">photo3</div>
           </div>
--- src/constants/beforeAfter.js
+++ src/constants/beforeAfter.js
@@ -5,4 +5,4 @@ import after2 from '../theme/assets/after2.jpg'
 import before3 from '../theme/assets/before3.jpg'
 import after3 from '../theme/assets/after3.jpg'
 
-export const BA_PICTURES= [before1, after1, before2, after2, before3, after3]
+export const BEFORE_AFTER_PICTURES= [before1, after1, before2, after2, before3, after3]
