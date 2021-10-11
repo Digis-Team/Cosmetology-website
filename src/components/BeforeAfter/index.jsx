8a4d1f51f7a607359f6410bb699eaecbfcfea58d
olhavalchak <o.valchak@do-it.co>
2021-10-11 10:38:50 +0300

    deleted comments

--- src/components/BeforeAfter/index.jsx
+++ src/components/BeforeAfter/index.jsx
@@ -22,27 +22,6 @@ export const BeforeAfter = () => {
             </div>
           )
         })}
-        {/* <div className="before-after-slider">
-          <ReactCompareSlider className="before-after-photos"
-            itemOne={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[0]} alt="before" />}
-            itemTwo={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[1]} alt="after" />}
-          />
-          <div className="before-after-text">Home care</div>
-        </div>
-        <div className="before-after-slider">
-          <ReactCompareSlider className="before-after-photos"
-            itemOne={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[2]} alt="before" />}
-            itemTwo={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[3]} alt="after" />}
-          />
-          <div className="before-after-text">Conical plastic</div>
-        </div>
-        <div className="before-after-slider">
-          <ReactCompareSlider className="before-after-photos"
-            itemOne={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[4]} alt="before" />}
-            itemTwo={<ReactCompareSliderImage src={BEFORE_AFTER_PICTURES[5]} alt="after" />}
-          />
-          <div className="before-after-text">Home care</div>
-        </div> */}
     </div>
   </div>
   )
