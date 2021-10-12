311de2e6318a36823f521c26b7a9ce40100ebe6f
olhavalchak <o.valchak@do-it.co>
2021-10-12 13:32:59 +0300

    fix and add constants in about me component

--- src/constants/aboutMe.js
+++ src/constants/aboutMe.js
@@ -2,25 +2,26 @@ export const ABOUT_ME_ICONS =[
   {
     key: '1',
     href: 'https://www.google.com/',
-    iClass: 'fa-user-nurse fas icon',
-    pText: 'Вища освіта'
+    iconClass: 'fa-user-nurse fas icon',
+    titleText: 'Вища освіта'
   },
   {
     key: '2',
     href: 'https://www.google.com/',
-    iClass: 'fa-user fas icon',
-    pText: 'Індивідуальний підхід до кожного'
+    iconClass: 'fa-user fas icon',
+    titleText: 'Індивідуальний підхід до кожного'
   },
   {
     key: '3',
     href: 'https://www.google.com/',
-    iClass: 'fa-arrow-up fas icon',
-    pText: 'Професійна косметика'
+    iconClass: 'fa-arrow-up fas icon',
+    titleText: 'Професійна косметика'
   },
   {
     key: '4',
     href: 'https://www.google.com/',
-    iClass: 'fa-map-marker-alt fas icon',
-    pText: 'Зручне розташування'
+    iconClass: 'fa-map-marker-alt fas icon',
+    titleText: 'Зручне розташування'
   },
 ];
+export const BUTTON_LINK = 'https://www.google.com/';
