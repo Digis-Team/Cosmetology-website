59a9b47d4965fdb145b09884d2433c11672d66ac
olhavalchak <90196305+olhavalchak@users.noreply.github.com>
2021-11-12 13:19:11 +0200

    Add logic to switch sections. (#11)
    * Add logic to switch sections.
    
    * Rename currentSection, add new lines.

--- src/constants/shop.js
+++ src/constants/shop.js
@@ -5,7 +5,7 @@ export const SHOP_ITEMS = [
     title: 'fruit',
     description: 'Very delicious fruit, juicy',
     price: '100$',
-    section: 'СИРОВАТКИ',
+    section: 1,
   },
   {
     id: 2,
@@ -13,7 +13,63 @@ export const SHOP_ITEMS = [
     title: 'fruit2',
     description: 'Very delicious fruit, juicy',
     price: '100$',
-    section: 'ЗВОЛОЖЕННЯ',
+    section: 2,
+  },
+  {
+    id: 3,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit3',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 1,
+  },
+  {
+    id: 4,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit4',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 1,
+  },
+  {
+    id: 5,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit5',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 2,
+  },
+  {
+    id: 6,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit6',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 1,
+  },
+  {
+    id: 7,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit7',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 2,
+  },
+  {
+    id: 8,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit8',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 1,
+  },
+  {
+    id: 9,
+    img: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    title: 'fruit9',
+    description: 'Very delicious fruit, juicy',
+    price: '100$',
+    section: 2,
   },
 ];
 export const SECTIONS = [
@@ -24,5 +80,29 @@ export const SECTIONS = [
   {
     id: 2,
     title: 'ЗВОЛОЖЕННЯ',
+  },
+  {
+    id: 3,
+    title: 'ПЕПТИДИ',
+  },
+  {
+    id: 4,
+    title: 'СОНЦЕЗАХИСТ',
+  },
+  {
+    id: 5,
+    title: 'ОЧИЩЕННЯ',
+  },
+  {
+    id: 6,
+    title: 'ВІДЛУЩУВАННЯ',
+  },
+  {
+    id: 7,
+    title: 'ТОНІКИ',
+  },
+  {
+    id: 8,
+    title: 'МАСКИ',
   },
 ];
