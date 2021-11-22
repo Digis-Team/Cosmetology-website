edfbac81f968d994f553059a8d5ca2cae7093511
olhavalchak <o.valchak@do-it.co>
2021-11-22 13:54:04 +0200

    Display items that were selected, add amount buttons.

--- src/pages/cartPage/CartPage.jsx
+++ src/pages/cartPage/CartPage.jsx
@@ -1,57 +1,36 @@
 import React from 'react';
 
-export const CartPage = () => (
-  <div className="cartlist-container">
-    <h1 className="zagolovok">Корзина</h1>
-    <div className="cart-items-container">
-      <div className="cart-item">
-        <img className="cart-img" src="https://u.makeup.com.ua/4/4x/4xp1kvrcf1ib.jpg" alt="item" />
-        <div className="item-description">
-          <h3 className="item-name">Антиоксидантна сироватка з ліпідним вітаміном С</h3>
-          <div className="price-amount">
-            <div className="price">Price: 100$</div>
-            <div className="amount">- 7 +</div>
-            <button className="cart-button delete-item" type="button">Видалити товар</button>
+export const CartPage = () => {
+  const cartItems = JSON.parse(localStorage.getItem('cartList'));
+  return (
+    <div className="cartlist-container">
+      <h1 className="zagolovok">Корзина</h1>
+      <div className="cart-items-container">
+        {cartItems.map((cartItem) => (
+          <div className="cart-item" key={cartItem.id}>
+            <img className="cart-img" src={cartItem.img} alt="item" />
+            <div className="item-description">
+              <h3 className="item-name">{cartItem.title}</h3>
+              <div className="price-amount">
+                <div className="price">
+                  <span>Price: $</span>
+                  {cartItem.price}
+                </div>
+                <div className="amount-container">
+                  <button className="cart-button" type="button">-</button>
+                  <span className="amount">1</span>
+                  <button className="cart-button" type="button">+</button>
+                </div>
+                <button className="cart-button delete-item" type="button">Видалити товар</button>
+              </div>
+            </div>
           </div>
+        ))}
+        <div className="make-order">
+          <button className="cart-button" type="button">Видалити все</button>
+          <button className="cart-button" type="button">Створити замовлення</button>
         </div>
-      </div>
-      <div className="cart-item">
-        <img className="cart-img" src="https://u.makeup.com.ua/4/4x/4xp1kvrcf1ib.jpg" alt="item" />
-        <div className="item-description">
-          <h3 className="item-name">Антиоксидантна сироватка з ліпідним вітаміном С</h3>
-          <div className="price-amount">
-            <div className="price">Price: 100$</div>
-            <div className="amount">- 7 +</div>
-            <button className="cart-button delete-item" type="button">Видалити товар</button>
-          </div>
-        </div>
-      </div>
-      <div className="cart-item">
-        <img className="cart-img" src="https://u.makeup.com.ua/4/4x/4xp1kvrcf1ib.jpg" alt="item" />
-        <div className="item-description">
-          <h3 className="item-name">Антиоксидантна сироватка з ліпідним вітаміном С</h3>
-          <div className="price-amount">
-            <div className="price">Price: 100$</div>
-            <div className="amount">- 7 +</div>
-            <button className="cart-button delete-item" type="button">Видалити товар</button>
-          </div>
-        </div>
-      </div>
-      <div className="cart-item">
-        <img className="cart-img" src="https://u.makeup.com.ua/4/4x/4xp1kvrcf1ib.jpg" alt="item" />
-        <div className="item-description">
-          <h3 className="item-name">Антиоксидантна сироватка з ліпідним вітаміном С</h3>
-          <div className="price-amount">
-            <div className="price">Price: 100$</div>
-            <div className="amount">- 7 +</div>
-            <button className="cart-button delete-item" type="button">Видалити товар</button>
-          </div>
-        </div>
-      </div>
-      <div className="make-order">
-        <button className="cart-button" type="button">Видалити все</button>
-        <button className="cart-button" type="button">Створити замовлення</button>
       </div>
     </div>
-  </div>
-);
+  );
+};
