9b3ad5167f31db47b08e65c6046ae0682e42a447
olhavalchak <o.valchak@do-it.co>
2021-12-01 16:27:05 +0200

    Create function which returns number of section that depends on points in test.

--- src/pages/skinTest/SkinTest.jsx
+++ src/pages/skinTest/SkinTest.jsx
@@ -4,15 +4,20 @@ import { QUESTIONS } from '../../constants';
 
 export const SkinTest = () => {
   const [points, setPoints] = useState(0);
-  let link = null;
-  const minimumPoints = QUESTIONS.length;
-  if (points <= minimumPoints) {
-    link = '/shop?section=1';
-  } else if (points <= minimumPoints * 2) {
-    link = '/shop?section=2';
-  } else {
-    link = '/shop?section=3';
-  }
+
+  const checkSectionFromPoins = () => {
+    const minimumPoints = QUESTIONS.length;
+    let link = null;
+    if (points <= minimumPoints) {
+      link = '/shop?section=1';
+    } else if (points <= minimumPoints * 2) {
+      link = '/shop?section=2';
+    } else {
+      link = '/shop?section=3';
+    }
+    return link;
+  };
+  const link = checkSectionFromPoins();
   return points === 0 ? <Questions setPoints={setPoints} />
     : <Results points={points} link={link} />;
 };
