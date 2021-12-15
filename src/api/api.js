0f9d95bfddafea3f9d4e794a674866e6176d2da2
olhavalchak <o.valchak@do-it.co>
2021-12-15 11:39:00 +0200

    Delete slash.

--- src/api/api.js
+++ src/api/api.js
@@ -3,11 +3,11 @@ import { root } from './config';
 
 export const api = Object.freeze({
   getShopItems: async (search) => {
-    const res = await axios.get(`${root}/shopItems/${search}`);
+    const res = await axios.get(`${root}/shopItems${search}`);
     return res.data;
   },
   getQuestions: async (search) => {
-    const res = await axios.get(`${root}/questions/${search}`);
+    const res = await axios.get(`${root}/questions${search}`);
     return res.data;
   },
 });
