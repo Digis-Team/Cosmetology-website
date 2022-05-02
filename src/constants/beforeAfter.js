0d16f20f363234d5d2870af798551519ca62aa87
Olha Valchak <o.valchak@do-it.co>
2022-05-02 16:37:55 +0300

    add files.

--- src/constants/beforeAfter.js
+++ src/constants/beforeAfter.js
@@ -2,8 +2,8 @@ import before1 from '../theme/assets/before1.jpg';
 import after1 from '../theme/assets/after1.jpg';
 import before2 from '../theme/assets/before2.jpg';
 import after2 from '../theme/assets/after2.jpg';
-import before3 from '../theme/assets/before3.jpg';
 import after3 from '../theme/assets/after3.jpg';
+import before31 from '../theme/assets/before3.jpg';
 
 export const BEFORE_AFTER_PICTURES = [
   {
@@ -20,7 +20,7 @@ export const BEFORE_AFTER_PICTURES = [
   },
   {
     id: 3,
-    before: before3,
+    before: before31,
     after: after3,
     text: 'Home care',
   },
